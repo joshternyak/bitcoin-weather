d35c762ac94e67253a0b488dc3463219d3ae4ecf
Josh Ternyak <joshternyak@gmail.com>
2021-04-22 09:15:32 -0500

    moon, sky css, current forecast

--- src/components/sky/sky.tsx
+++ src/components/sky/sky.tsx
@@ -3,13 +3,15 @@ import "./sky.scss"
 import Sun from "./sun/sun"
 import Cloud from "./cloud/cloud"
 import Rain from "./rain/rain"
+import Moon from "./moon/moon"
 
-export default function Sky() {
+export default function Sky({ theme, cloudy, night }: any) {
   return (
-    <div className="Sky">
-      <Sun />
-      <Cloud />
-      {false && <Rain />}
+    <div className={`Sky Sky--${theme} ${night ? "Sky--night" : ""} `}>
+      {theme === "sunny" && !night && <Sun />}
+      {night && <Moon />}
+      {cloudy && <Cloud theme={theme} night={night} />}
+      {theme === "storm" && <Rain />}
     </div>
   )
 }
