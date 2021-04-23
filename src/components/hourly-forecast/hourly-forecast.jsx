a5e901f3d6111c90799d7869373d778e02b065ca
Josh Ternyak <joshternyak@gmail.com>
2021-04-23 10:04:18 -0500

    made hourly-forecast component scrollable on drag

--- src/components/hourly-forecast/hourly-forecast.jsx
+++ src/components/hourly-forecast/hourly-forecast.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import "./hourly-forecast.scss"
 import hourlyForecastData from "../../hourlyForecast"
 import HourWeatherItem from "../hour-weather-item/hour-weather-item"
+import ScrollContainer from "react-indiana-drag-scroll"
 
 export default function HourlyForecast() {
   return (
-    <div className="HourlyForecast">
+    <ScrollContainer className="scroll-container HourlyForecast">
       {hourlyForecastData.map((day, index) => (
         <HourWeatherItem key={index} price={day.price} />
       ))}
-    </div>
+    </ScrollContainer>
   )
 }
