ac50c4fe985667ee4295c5b090f86a7d4c04176c
braydentw <reshiramfire130@gmail.com>
2021-04-25 11:28:16 -0500

    Added functionality to display hours

--- src/components/hour-weather-item/hour-weather-item.tsx
+++ src/components/hour-weather-item/hour-weather-item.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import "./hour-weather-item.scss"
 
-export default function HourWeatherItem({ price, icon }: any) {
+export default function HourWeatherItem({ price, icon, displayTime }: any) {
   return (
     <div className="HourWeatherItem">
-      <p className="HourWeatherItem__time">Now</p>
+      <p className="HourWeatherItem__time">{displayTime}</p>
       <div className="HourWeatherItem__weather-icon">
         <img src={icon} alt="icon" />
       </div>
